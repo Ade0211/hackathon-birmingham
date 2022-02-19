91991b2588554e53d928ed5edf959b20cd0d1f24
Ade0211 <debbyinka21@gmail.com>
2022-02-19 17:01:05 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+// import AppRouter from "./AppRouter";
+// import Home from "./Home";
+import Carousel from "./Component/Carousel"
 
 function App() {
+  // const [infoData, setInfoData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/posts/users/list")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+
+  //       // let result = data.map(a => a.file_mimetype);
+  //       console.log(data)
+  //       setInfoData(data)
+
+  //     })
+  // }, [])
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Carousel />
     </div>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/Helpers/CarouselData.js
@@ -0,0 +1,9 @@
+import english_problem from "../Images/english_problem.png";
+import govpaper_problem from "../Images/govpaper_problem.png";
+import mail_problem from "../Images/mail_problem.png";
+
+export const images = [
+    { title: "San Diego", subtitle: "This is San Diego", img: english_problem },
+    { title: "ILR", subtitle: "This is paper problem", img: govpaper_problem },
+    { title: "Mail", subtitle: "This is mail problem", img: mail_problem }
+]
